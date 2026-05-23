0419077b60063b88704909f10f31fb801e55becd
Yuta Hayashibe <yuta@hayashibe.jp>
2026-05-23 23:21:16 +0900

    タイトル表示

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import TocToggle from './TocToggle.vue'
+import SearchButton from './SearchButton.vue'
 import './custom.css'
 
 export default {
@@ -8,6 +9,7 @@ export default {
     Layout: () => {
         return h(DefaultTheme.Layout, null, {
             'nav-bar-content-before': () => h(TocToggle),
+            'nav-bar-content-after': () => h(SearchButton),
         })
     },
 }
