db9f2f7bfb4b8e352d178d20a366a5698174ad90
Yuta Hayashibe <yuta@hayashibe.jp>
2026-08-14 00:03:21 +0900

    fix: quiz本文リンクのslug検証をVitePressに合わせる

--- site/scripts/check_quiz.mjs
+++ site/scripts/check_quiz.mjs
@@ -26,10 +26,18 @@ function hasMath(source) {
 }
 
 function slugifyHeading(text) {
+    const rControl = /[\u0000-\u001f]/g
+    const rSpecial = /[\s~`!@#$%^&*()\-_+=[\]{}|\\;:"'“”‘’<>,.?/]+/g
+    const rCombining = /[\u0300-\u036F]/g
     return text
+        .normalize('NFKD')
+        .replace(rCombining, '')
+        .replace(rControl, '')
+        .replace(rSpecial, '-')
+        .replace(/-{2,}/g, '-')
+        .replace(/^-+|-+$/g, '')
+        .replace(/^(\d)/, '_$1')
         .toLowerCase()
-        .replace(/\s+/g, '-')
-        .replace(/[^\p{L}\p{N}\-]/gu, '')
 }
 
 function sourceFile(href) {
